13e338dae7beec11827b3b9697c1239b381dce67
krystal <164693817@qq.com>
2017-11-23 21:22:10 +0800

    实现登陆+session

--- app.js
+++ app.js
@@ -58,8 +58,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
+var flash = require('connect-flash');
 
+var session = require('express-session');
+//创建mongo和session回话机制
+var MongoStore = require('connect-mongo')(session);
+
+var routes = require('./routes/index');
 var app = express();
 
 // 设置模板目录
@@ -72,10 +77,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+// secret:一个String类型的字符串，作为服务器端生成session的签名。
+// name:返回客户端的key的名称，默认为connect.sid,也可以自己设置。
+// resave:(是否允许)当客户端并行发送多个请求时，其中一个请求在另一个请求结束时对session进行修改覆盖并保存。
+//
+// 默认为true。但是(后续版本)有可能默认失效，所以最好手动添加。
+//
+// saveUninitialized:初始化session时是否保存到存储。默认为true， 但是(后续版本)有可能默认失效，所以最好手动添加。
+//
+// cookie:设置返回到前端key的属性，默认值为{ path: ‘/’, httpOnly: true, secure: false, maxAge: null }。
+//
+//     express-session的一些方法:
+//
+//     Session.destroy():删除session，当检测到客户端关闭时调用。
+//
+// Session.reload():当session有修改时，刷新session。
+//
+// Session.regenerate()：将已有session初始化。
+//
+// Session.save()：保存session。
+app.use(session({
+    name: "blog",// 设置 cookie 中保存 session id 的字段名称
+    secret: "blog",// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+    cookie: {maxAge: 6000000},// 过期时间，过期后 cookie 中的 session id 自动删除
+    store:new MongoStore({url:'mongodb://localhost/mongodb'}),//将session储存到mongodb中
+    resave: false,
+    saveUninitialized: true
+}));
+
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/lib",express.static(path.join(__dirname, 'node_modules')));
 
+//flash
+app.use(flash());
+//设置flash
+app.use(function(req, res, next){
+    res.locals.user = req.session.user;//设置整站全局变量，一般存放用户全局信息，session和一些整站配置变量
+    res.locals.error = req.flash('error') || "";
+    res.locals.success = req.flash('success') || "";
+    next();
+});
 // 设置路由
 routes(app);
 
@@ -83,7 +126,7 @@ routes(app);
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
-    next(err);
+    next(err);//对于相同的挂载路径可以挂载多个中间件，因为路径的相同，调用next的时候会自动执行下一个匹配相同路径的中间件
 });
 
 // error handler
--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,12 @@ module.exports = function(app) {
             title:'发表文章',
         });
     });
+    //退出登录
+    app.get('/logout',function (req, res) {
+        req.session.user = null;
+        req.flash('success', '登出成功!');
+        res.redirect('/');//登出成功后跳转到主页
+    });
 
     //来自/reg的post请求
     app.post('/reg', function (req, res) {
@@ -35,7 +41,7 @@ module.exports = function(app) {
         });
         if(req.body['password'] != req.body['password-repeat']){
 
-            //req.flash("error",'两次输入的密码不一致');
+            req.flash("error",'两次输入的密码不一致');
             console.log('两次输入的密码不一致');
             return res.redirect('/');//返回注册页
         }
@@ -46,18 +52,18 @@ module.exports = function(app) {
                 return res.redirect('/');
             }
             if(data != null){
-                // req.flash('error','该用户已存在');
+                req.flash('error','该用户已存在');
                 console.log('该用户已存在');
                 return res.redirect('/reg');//返回注册页
             }else{
                 //保存新的用户
                 user.save(function(err){
                     if(err){
-                        //req.flash('err',err);
+                        req.flash('err',err);
                         console.log(err);
                         return res.redirect('/');
                     }
-                    //req.flash('success', '注册成功!');
+                    req.flash('success', '注册成功!');
                     console.log('注册用户成功');
                     res.redirect('/');//注册成功后返回主页
                 })
@@ -70,23 +76,25 @@ module.exports = function(app) {
         User.findOne({'username':req.body.username},function(err,user){
             if(err){
                 console.log('error','err');
-                // req.flash('error','登录出错');
+                req.flash('error','登录出错');
                 return res.redirect('/');
             }
             //用户不存在
             if(!user){
                 console.log('error','用户不存在');
-                // req.flash('error','用户不存在');
+                req.flash('error','用户不存在');
                 return res.redirect('/login');
             }
             //判断密码是否一致
-            if(user.username != password){
+            if(user.password != password){
                 console.log('error','密码错误');
-                 // req.flash('error','密码错误');
+                 req.flash('error','密码错误');
                 return res.redirect('/');
             }
+
+            req.session.user = user;
             console.log(user.username);
-            // req.flash('success','登录成功');
+            req.flash('success','登录成功');
             res.redirect('/');
         });
     });
